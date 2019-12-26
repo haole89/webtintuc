84d3673954faee2bb8160facf344889563d8b259
Administrator <Administrator@HAOLE>
2019-12-26 16:27:32 +0700

    Khởi tạo dự án

---
+++ HelloWordK20A/HelloWordK20A/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HelloWordK20A.Models;
+
+namespace HelloWordK20A.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ViewResult Index()
+        {
+            int gio = 0;
+            gio = DateTime.Now.Hour;
+            if (gio < 12)
+                ViewBag.xinchao = "Chào buổi sáng";
+            else
+                ViewBag.xinchao = "Chào buổi chiều";
+
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult XuLy()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public ViewResult XuLy(GuestResponse khachmoi)
+        {
+            return View("HoanThanh", khachmoi);
+        }        
+    }
+}
---
+++ HelloWordK20A/HelloWordK20A/Models/GuestResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HelloWordK20A.Models
+{
+    public class GuestResponse
+    {
+        public string HoTen { get; set; }
+        public string Email { get; set; }
+        public string DienThoai { get; set; }
+        public bool XacNhan { get; set; }
+    }
+}
